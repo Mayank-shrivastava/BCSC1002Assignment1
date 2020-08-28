7845f4c8115cb33409e48d3eabd4df527f53001d
mayank <mayank.shrivastava_cs19@gla.ac.in>
2020-08-28 22:53:57 +0530

    create getters and setters for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -23,4 +23,36 @@ public class Student {
             issuedBooks[i] = new Book();
         }
     }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    public int getNumberOfBooksIssued() {
+        return numberOfBooksIssued;
+    }
+
+    public void setNumberOfBooksIssued(int numberOfBooksIssued) {
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
+    public Book[] getIssuedBooks() {
+        return issuedBooks;
+    }
+
+    public void setIssuedBooks(Book[] issuedBooks) {
+        this.issuedBooks = issuedBooks;
+    }
 }
