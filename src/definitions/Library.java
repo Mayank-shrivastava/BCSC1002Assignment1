8899ba8b2a6da2088986facd9e585cf530b1104f
mayank <mayank.shrivastava_cs19@gla.ac.in>
2020-08-28 23:07:38 +0530

    create constructors for the Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,5 +7,17 @@
 package definitions;
 
 public class Library {
+    private static final int NUMBER_OF_BOOKS_AVAILABLE = 10;
     private Book[] currentAvailableBooks;
+
+    public Library(Book[] currentAvailableBooks) {
+        this.currentAvailableBooks = currentAvailableBooks;
+    }
+
+    public Library() {
+        this.currentAvailableBooks = new Book[NUMBER_OF_BOOKS_AVAILABLE];
+        for (int i = 0; i < NUMBER_OF_BOOKS_AVAILABLE; i++) {
+            currentAvailableBooks[i] = new Book();
+        }
+    }
 }
